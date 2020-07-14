942033069a43e4ca0d52d7ffd3a18b57a8027e3f
Andy Cyca <andycyca@gmail.com>
2020-07-14 13:49:41 -0500

    Update pye2.py
    Refactored the script to run from a single main function. Now accepts a few options (path, version, drafts, logs, username)
    
    `output` is a CLI option but it's not implemented yet

--- pye2.py
+++ pye2.py
@@ -19,6 +19,7 @@ Requirements:
 - palettable
 """
 
+# %% Front matter
 __author__ = 'Andycyca'
 __copyright__ = 'Copyright 2020'
 __credits__ = ['Andycyca']
@@ -33,6 +34,47 @@ from os.path import join, getsize
 import re
 import matplotlib.pyplot as plt
 from palettable.tableau import Tableau_20 as my_cmap
+import argparse
+
+# %% Parsing information
+
+# Initiate the parser
+parser = argparse.ArgumentParser()
+
+# Define the program description
+text = '''
+Basic visualization and analysis for Everything2 nodes,
+using purely public data'''
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is invalid")
+
+
+# Initiate the parser with a description
+parser = argparse.ArgumentParser(description=text)
+parser.add_argument('path',
+                    type=dir_path,
+                    help='path to your html files')
+parser.add_argument('-v', '--version',
+                    help='show program version', action='store_true')
+parser.add_argument('-d', '--drafts',
+                          help='turn on drafts in visualization',
+                          action='store_true')
+parser.add_argument('-l', '--logs',
+                          help='turn on logs in visualization',
+                          action='store_true')
+parser.add_argument('-u', '--username',
+                    help='a username for display',
+                    default='')
+parser.add_argument('-o', '--output',
+                    help='output directory')
+args = parser.parse_args()
+
+# %% Discrete main functions
 
 
 def trim_extension(filename):
@@ -75,72 +117,105 @@ def get_title_and_type(filename):
 
 
 def get_node_data(path):
+    '''
+    Generates data from files in 'path'
+
+    Parameters
+    ----------
+    path : str
+        Path to html files.
+
+    Returns
+    -------
+    nodecount : dict
+        A dict with node types and frequency count.
+    nodedata : List
+        List of arrays. Every entry has [node, type, size] information.
+
+    '''
+    nodecount = {}
+    nodedata = []
     for root, dirs, files in os.walk(path):
         for filename in files:
             name = trim_extension(filename)
             title, nodetype = get_title_and_type(name)
-            node_data.append([title, nodetype, getsize(join(root, filename))])
-            if nodetype not in node_count:
-                node_count[nodetype] = 1
+            nodedata.append([title, nodetype, getsize(join(root, filename))])
+            if nodetype not in nodecount:
+                nodecount[nodetype] = 1
             else:
-                node_count[nodetype] += 1
+                nodecount[nodetype] += 1
     # Minor sanitization
-    if "unparseable" in node_count:
-        del node_count['unparseable']
+    if "unparseable" in nodecount:
+        del nodecount['unparseable']
 
-    if without_drafts:
-        del node_count['draft']
+    if not args.drafts:
+        del nodecount['draft']
 
-    if without_logs:
-        del node_count['log']
+    if not args.logs:
+        del nodecount['log']
+
+    return nodecount, nodedata
 
 
-# Begin here
-p = 'C:\\Users\\craze\\Documents\\Python\\e2'
+def create_graphs(sortedlist, totalnodes, nodecount, nodedata):
+    # For visualization purposes
+    my_xticks = []
+    for idx, _ in enumerate(sortedlist):
+        my_xticks.append(sortedlist[idx][0])
 
-# Set to hold raw data
-node_count = {}
-node_data = []
-without_drafts = False
-without_logs = False
+    # Fig 1: all nodes, by type (includes drafts)
+    fig, ax = plt.subplots()
+    if args.username:
+        plt.title('Noding distribution for ' + args.username)
+    else:
+        plt.title('Noding distribution for user')
+    ax.set_prop_cycle('color', my_cmap.mpl_colors)
+    for idx, name in enumerate(sortedlist):
+        plt.bar(idx, sortedlist[idx][1])
+    plt.xticks(range(len(nodecount)), labels=my_xticks, rotation='vertical')
+    plt.xlabel('Node types')
+    plt.ylabel('Frequency (absolute)')
+    plt.savefig('nodesbytype.png')
 
-# Process the whole thing
-get_node_data(p)
+    # Fig 2: All node sizes
+    fig, ax = plt.subplots()
+    ax.set_yscale('log')
+    if args.username:
+        plt.title('Node sizes for ' + args.username)
+    else:
+        plt.title('Node sizes for user')
+    ax.set_prop_cycle('color', my_cmap.mpl_colors)
+    plt.scatter(range(totalnodes),
+                [nodedata[i][2] for i in range(totalnodes)],
+                marker='.')
+    plt.xlabel('Node rating')
+    plt.ylabel('Size (bytes)')
+    plt.savefig('nodesbysize.png')
+    plt.show()
 
-# For easier counting
-node_types = len(node_count)
-total_nodes = len(node_data)
 
-# Sort by number of nodes in descending order
-sorted_list = sorted(node_count.items(), key=lambda kv: kv[1], reverse=True)
+# %% Main function
+def main(p=args.path):
 
-# Sort by node size (approximate)
-node_data = sorted(node_data, key=lambda size: size[2], reverse=True)
+    if args.version:
+        print('python visualization program for everything2, v.20200713')
 
-# For visualization purposes
-my_xticks = []
-for idx, _ in enumerate(sorted_list):
-    my_xticks.append(sorted_list[idx][0])
+    # Process the whole thing
+    node_count, node_data = get_node_data(p)
 
-# Fig 1: all nodes, by type (includes drafts)
-fig, ax = plt.subplots()
-plt.title('Noding distribution for user')
-ax.set_prop_cycle('color', my_cmap.mpl_colors)
-for idx, name in enumerate(sorted_list):
-    plt.bar(idx, sorted_list[idx][1])
-plt.xticks(range(node_types), labels=my_xticks, rotation='vertical')
-plt.xlabel('Node types')
-plt.ylabel('Frequency (absolute)')
-plt.savefig('nodesbytype.png')
+    # For easier counting
+    node_types = len(node_count)
+    total_nodes = len(node_data)
 
-# Fig 2: All node sizes
-fig, ax = plt.subplots()
-ax.set_yscale('log')
-plt.title('Node sizes')
-ax.set_prop_cycle('color', my_cmap.mpl_colors)
-plt.scatter(range(total_nodes), [node_data[i][2] for i in range(total_nodes)],
-            marker='.')
-plt.xlabel('Node rating')
-plt.ylabel('Size (bytes)')
-plt.savefig('nodesbysize.png')
-plt.show()
+    # Sort by number of nodes in descending order
+    sorted_list = sorted(node_count.items(),
+                         key=lambda kv: kv[1], reverse=True)
+
+    # Sort by node size (approximate)
+    node_data = sorted(node_data, key=lambda size: size[2], reverse=True)
+
+    create_graphs(sorted_list, total_nodes, node_count, node_data)
+
+
+if __name__ == "__main__":
+    main()
